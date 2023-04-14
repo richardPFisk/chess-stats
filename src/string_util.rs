606df1c00208c6eb949aef393c24b8b07709bc8f
Richard Fisk <richard.fisk@dabble.com.au>
2023-04-14 17:06:02 +1000

    working tests

--- src/string_util.rs
+++ src/string_util.rs
@@ -1,14 +1,17 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{BTreeMap, HashMap, HashSet};
 
 fn is_special_sub_string(s1: &str, s2: &str) -> bool {
     let replaced_string = s2.replace(s1, "");
     replaced_string.len() < s2.len()
 }
 
-pub fn get_parent_child_strings(some_strings: Vec<String>) -> HashMap<String, Vec<String>> {
-    let mut map = HashMap::new();
+pub fn get_parent_child_strings(some_strings: Vec<String>) -> BTreeMap<String, Vec<String>> {
+    let mut strings = some_strings.clone();
+    strings.sort();
 
-    for s in some_strings {
+    let mut map = BTreeMap::new();
+
+    for s in strings {
         let parts: Vec<_> = s.split("-").collect();
         let parent = parts[0..2].join("-");
 
@@ -26,31 +29,31 @@ pub fn get_parent_child_strings(some_strings: Vec<String>) -> HashMap<String, Ve
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
     use super::get_parent_child_strings;
+    use pretty_assertions::{assert_eq, assert_ne};
+    use std::collections::{BTreeMap, HashMap};
 
     #[test]
     fn test_parent_child() {
         let openings = [
-            "Queens-Gambit-Declined-Marshall-Defense...4.e4-Nf6-5.Nc3-e6",
-            "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
-            "Ruy-Lopez-Opening-Classical-Central-Variation",
-            "French-Defense-Exchange-Variation...4.Bd3-c5-5.dxc5-Bxc5",
-            "Queens-Pawn-Opening-Accelerated-London-Steinitz-Countergambit",
-            "Sicilian-Defense-French-Variation",
-            "French-Defense-Exchange-Variation...4.Nf3-Nf6-5.Bd3-c5",
-            "Modern-Defense-with-1-d4-2.c4-Bg7-3.Nc3",
-            "Queens-Gambit-Declined-Queens-Knight-Variation-3...Nf6",
-            "Kings-Pawn-Opening...2.Qe2-Nc6-3.c3-Nf6-4.Nf3",
+            "French-Defense-Tarrasch-Open-Delayed-Exchange-Variation",
             "French-Defense-Advance-Variation-3...c5-4.c3-Nc6",
             "French-Defense-Advance-Variation-3...c5-4.c3",
             "French-Defense-Tarrasch-Open-Euwe-Keres-Line-4...Nc6",
+            "French-Defense-Exchange-Variation...4.Bd3-c5-5.dxc5-Bxc5",
+            "French-Defense-Knight-Variation-2...d5-3.exd5-exd5-4.d4",
+            "French-Defense-Exchange-Variation...4.Nf3-Nf6-5.Bd3-c5",
+            "Queens-Gambit-Declined-Marshall-Defense...4.e4-Nf6-5.Nc3-e6",
+            "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
+            "Ruy-Lopez-Opening-Classical-Central-Variation",
+            "Queens-Pawn-Opening-Accelerated-London-Steinitz-Countergambit",
+            "Sicilian-Defense-French-Variation",
+            "Modern-Defense-with-1-d4-2.c4-Bg7-3.Nc3",
+            "Queens-Gambit-Declined-Queens-Knight-Variation-3...Nf6",
+            "Kings-Pawn-Opening...2.Qe2-Nc6-3.c3-Nf6-4.Nf3",
             "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
             "Ruy-Lopez-Opening",
             "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
-            "French-Defense-Knight-Variation-2...d5-3.exd5-exd5-4.d4",
-            "French-Defense-Tarrasch-Open-Delayed-Exchange-Variation",
             "Scandinavian-Defense-Mieses-Kotrc-Gubinsky-Melts-Defense-4.Nf3",
         ]
         .into_iter()
@@ -59,12 +62,35 @@ mod tests {
 
         let result = get_parent_child_strings(openings);
         assert_eq!(
-              result,
-            HashMap::from_iter(
+            result,
+            BTreeMap::from_iter(
                 [
+                    (
+                        "French-Defense",
+                        vec![
+                            "French-Defense-Advance-Variation-3...c5-4.c3",
+                            "French-Defense-Advance-Variation-3...c5-4.c3-Nc6",
+                            "French-Defense-Exchange-Variation...4.Bd3-c5-5.dxc5-Bxc5",
+                            "French-Defense-Exchange-Variation...4.Nf3-Nf6-5.Bd3-c5",
+                            "French-Defense-Knight-Variation-2...d5-3.exd5-exd5-4.d4",
+                            "French-Defense-Tarrasch-Open-Delayed-Exchange-Variation",
+                            "French-Defense-Tarrasch-Open-Euwe-Keres-Line-4...Nc6",
+                        ]
+                    ),
+                    (
+                        "Kings-Pawn",
+                        vec!["Kings-Pawn-Opening...2.Qe2-Nc6-3.c3-Nf6-4.Nf3"]
+                    ),
                     (
                         "Modern-Defense",
                         vec!["Modern-Defense-with-1-d4-2.c4-Bg7-3.Nc3"]
+                    ),
+                    (
+                        "Queens-Gambit",
+                        vec![
+                            "Queens-Gambit-Declined-Marshall-Defense...4.e4-Nf6-5.Nc3-e6",
+                            "Queens-Gambit-Declined-Queens-Knight-Variation-3...Nf6"
+                        ]
                     ),
                     (
                         "Queens-Pawn",
@@ -75,40 +101,17 @@ mod tests {
                         vec!["Sicilian-Defense-French-Variation"]
                     ),
                     (
-                        "French-Defense",
-                        vec![
-                            "French-Defense-Exchange-Variation...4.Bd3-c5-5.dxc5-Bxc5",
-                            "French-Defense-Exchange-Variation...4.Nf3-Nf6-5.Bd3-c5",
-                            "French-Defense-Advance-Variation-3...c5-4.c3-Nc6",
-                            "French-Defense-Advance-Variation-3...c5-4.c3",
-                            "French-Defense-Tarrasch-Open-Euwe-Keres-Line-4...Nc6",
-                            "French-Defense-Knight-Variation-2...d5-3.exd5-exd5-4.d4",
-                            "French-Defense-Tarrasch-Open-Delayed-Exchange-Variation"
-                        ]
-                    ),
-                    (
                         "Scandinavian-Defense",
                         vec!["Scandinavian-Defense-Mieses-Kotrc-Gubinsky-Melts-Defense-4.Nf3"]
                     ),
                     (
                         "Ruy-Lopez",
                         vec![
-                            "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
+                            "Ruy-Lopez-Opening",
                             "Ruy-Lopez-Opening-Classical-Central-Variation",
-                            "Ruy-Lopez-Opening"
+                            "Ruy-Lopez-Opening-Cozio-Defense-4.c3-a6-5.Ba4",
                         ]
                     ),
-                    (
-                        "Kings-Pawn",
-                        vec!["Kings-Pawn-Opening...2.Qe2-Nc6-3.c3-Nf6-4.Nf3"]
-                    ),
-                    (
-                        "Queens-Gambit",
-                        vec![
-                            "Queens-Gambit-Declined-Marshall-Defense...4.e4-Nf6-5.Nc3-e6",
-                            "Queens-Gambit-Declined-Queens-Knight-Variation-3...Nf6"
-                        ]
-                    )
                 ]
                 .iter()
                 .map(|(k, v)| (
@@ -134,7 +137,7 @@ mod tests {
         let result = get_parent_child_strings(openings);
         assert_eq!(
             result,
-            HashMap::from_iter(
+            BTreeMap::from_iter(
                 [(
                     "Ruy-Lopez",
                     vec![
