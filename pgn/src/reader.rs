a5da988a60650cb2a2d829d963ef7b378a1f153c
Richard Fisk <richard.fisk@dabble.com.au>
2024-06-30 17:07:06 +1000

    pgn

---
+++ pgn/src/reader.rs
@@ -0,0 +1,23 @@
+use pgn_reader::BufferedReader;
+use std::{borrow::Cow, collections::HashMap, io};
+
+use crate::models::{HeaderVisitor, MoveCounter};
+
+pub fn read_pgn_headers(pgn: &str) -> io::Result<Option<HashMap<Cow<str>, Cow<str>>>> {
+    let mut reader = BufferedReader::new_cursor(&pgn[..]);
+
+    let mut header_visitor = HeaderVisitor::new();
+    let headers = reader.read_game(&mut header_visitor)?;
+
+    Ok(headers)
+}
+
+pub fn read_pgn(pgn: &str) -> io::Result<()> {
+    let mut reader = BufferedReader::new_cursor(&pgn[..]);
+
+    let mut counter = MoveCounter::new();
+    let moves = reader.read_game(&mut counter)?;
+    println!("{moves:#?}");
+
+    Ok(())
+}
--- ui/src/engine.rs
+++
@@ -1,5 +0,0 @@
-pub mod pgn;
-pub mod bnf_pgn;
-pub mod pgn_reader;
-
-
