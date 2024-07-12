3b57fc9b741b39036eb4156b8e5d3091873399f5
Richard Fisk <richard.fisk@dabble.com.au>
2024-07-12 10:08:06 +1000

    some sense of movement

--- ui/src/board.rs
+++ ui/src/board.rs
@@ -26,48 +26,79 @@ pub fn ChessBoard(props: ChessBoardComponentProps) -> Element {
 
     let mut dragged_piece: Signal<Option<(usize, usize)>> = use_signal(|| None);
     let mut dropped_piece: Signal<Option<(usize, usize)>> = use_signal(|| None);
+    let mut drag_position: Signal<Option<(f64, f64)>> = use_signal(|| None);
+
+  //   let style_attr = use_memo(move || {
+  //     let dragged = dragged_piece.read();
+  //     let position = drag_position.read();
+  //     if let (Some((r, f)), Some((x, y))) = (*dragged, *position) {
+  //         if r == rank && f == file {
+  //             format!("transform: translate({}px, {}px);", x, y)
+  //         } else {
+  //             String::new()
+  //         }
+  //     } else {
+  //         String::new()
+  //     }
+  // });
 
     rsx! {
         style { {include_str!("./assets/drag-drop.css")} }
         style { {include_str!("./assets/board.css")} }
         div {
-          class: "chess-board",
-          "{dragged_piece:?}{dropped_piece:?}"
+            class: "chess-board",
+            "{dragged_piece:?}{dropped_piece:?}"
 
-        div {
-          class: "board",
+            div {
+                class: "board",
 
-          for rank in (0..8).rev() {
-            for file in 0..8 {
-                div { class: if (rank + file) % 2 == 1 { "piece-square light-square" } else { "piece-square dark-square" },
-                span {
-                  class: "chess-piece",
-                  key: "{file},{rank}",
-                  prevent_default: "ondragover ondrop",
-                  draggable: true,
-                    ondrop: move |_| {
-                      if let (Some(from), Some(to)) = (dragged_piece.read().clone(), dropped_piece.read().clone()) {
-                        tracing::info!("####### Drop ####### ({from:?},{to:?})");
-                        // make_move(from, to);
-                      }
-                    },
-                    ondragover: move |_| {
-                      dropped_piece.set(Some((rank, file)));
-                      // tracing::info!("Drag ondragover for  ({rank},{file}) {event:#?} ", );
-                      // tracing::info!("ondragover");
-                    },
-                    ondragstart: move |event: DragEvent| {
-                      dragged_piece.set(Some((rank, file)));
-                      let coords: Coordinates = event.coordinates();
-                      // tracing::info!("Drag started at: {:?}", coords);
-
-                    },
-                    PieceComponent { board: board.clone(), rank, file }
+                for rank in (0..8).rev() {
+                    for file in 0..8 {
+                        div {
+                            class: if (rank + file) % 2 == 1 { "piece-square light-square" } else { "piece-square dark-square" },
+                            span {
+                                class: "chess-piece",
+                                key: "{file},{rank}",
+                                prevent_default: "ondragover ondrop",
+                                draggable: true,
+                                ondrop: move |_| {
+                                    if let (Some(from), Some(to)) = (dragged_piece.read().clone(), dropped_piece.read().clone()) {
+                                        tracing::info!("####### Drop ####### ({from:?},{to:?})");
+                                        // make_move(from, to);
+                                    }
+                                    drag_position.set(None);
+                                },
+                                ondragover: move |_| {
+                                    dropped_piece.set(Some((rank, file)));
+                                },
+                                ondragstart: move |event: DragEvent| {
+                                    dragged_piece.set(Some((rank, file)));
+                                    let coords: Coordinates = event.coordinates();
+                                    drag_position.set(Some((coords.client().x as f64, coords.client().y as f64)));
+                                },
+                                ondrag: move |event: DragEvent| {
+                                    let coords: Coordinates = event.coordinates();
+                                    drag_position.set(Some((coords.client().x as f64, coords.client().y as f64)));
+                                },
+                                style: use_memo(move || {
+                                  let dragged = dragged_piece.read();
+                                  let position = drag_position.read();
+                                  if let (Some((r, f)), Some((x, y))) = (*dragged, *position) {
+                                      if r == rank && f == file {
+                                          format!("transform: translate({}px, {}px);", x, y)
+                                      } else {
+                                          String::new()
+                                      }
+                                  } else {
+                                      String::new()
+                                  }
+                              }),
+                                PieceComponent { board: board.clone(), rank, file }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
-          }
         }
     }
 }
